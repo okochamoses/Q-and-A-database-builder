a2aeeac13356fd491b40876086325417e9466a88
Moses Okocha <okocha.chukwutem@gmail.com>
2018-07-15 23:07:12 +0100

    Add GET '/' route

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+
+const routes = require("./routes");
 
 const app = express();
+
+// Register partials
+hbs.registerPartials(__dirname + "views/partials");
 
 // define express middleware
 app.use(express.static(__dirname + "public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
-});
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
